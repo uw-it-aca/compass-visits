6cfde68831dedd5f28d04badd359f37b73a87882
Stephen De Vight <devights@gmail.com>
2026-03-27 16:01:55 -0700

    add pws calls

---
+++ compass_visits/dao/pws.py
@@ -0,0 +1,28 @@
+# Copyright 2026 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+from uw_pws import PWS
+
+
+PHOTO_SIZE = "large"
+
+
+def get_student_profile(uwnetid):
+    pws = PWS()
+    person = pws.get_person_by_netid(uwnetid)
+    if person is None:
+        return None
+    return {
+        "netid": uwnetid,
+        "student_name": person.display_name,
+        "student_number": person.student_number,
+    }
+
+
+def get_student_photo(uwnetid):
+    pws = PWS()
+    person = pws.get_person_by_netid(uwnetid)
+    if person is None:
+        return None
+
+    return pws.get_idcard_photo(person.uwregid, size=PHOTO_SIZE)
