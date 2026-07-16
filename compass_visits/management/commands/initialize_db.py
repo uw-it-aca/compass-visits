b34e56d8297ac85f980bbc193ce448c7a0475f10
Stephen De Vight <devights@uw.edu>
2026-07-16 16:50:05 -0700

    lint failure

--- compass_visits/management/commands/initialize_db.py
+++ compass_visits/management/commands/initialize_db.py
@@ -4,6 +4,10 @@
 
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
+#test lint failure
+import json
+import datetime
+foo = "bar"
 
 
 class Command(BaseCommand):
